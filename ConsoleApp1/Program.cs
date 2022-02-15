3b0e2091d281316ba858b821dfc6ca6da613e53b
Ilfat <ilfat95@mail.ru>
2022-02-15 21:50:35 +0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+/// <summary>
+/// создать расширяющий метод для целочисленного массива, который сортирует элементы массива по возрастанию.
+/// </summary>
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] arr = { 5, -20, 4, 3,10, 2, 1 };
+            arr.Print();
+            arr.Sort();
+            arr.Print();
+
+            Console.ReadKey();
+        }
+    }
+
+    static class ExtenshionInt
+    {
+        public static int[] Sort(this int[] array)
+        {
+            int f;
+            int temp;
+            for (int i = 1; i < array.Length; i++)
+            {
+                f = 0;
+                for (int j = 0; j < array.Length-i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        f = 1;
+                    }
+                }
+                if (f == 0) break;
+
+            }
+            return array;
+        }
+        public static void Print(this int[] arr)
+        {
+            foreach (int item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,58 @@
+﻿
+/// <summary>
+/// Создать класс Note, представляющий музыкальную ноту. Класс Note должен содержать следующие данные: частота, продолжительность в миллисекундах и значение типа bool, указывающее, является ли нота паузой. Создать 7 фабричных методов для создания нот (до, ре, ми, фа, соль, ля, си), фабричные методы в качестве параметров должны принимать номер октавы для соответствующей ноты и значение продолжительности в миллисекундах. Создать статический класс MelodyParser с методом для преобразовния строки из цифр и пробелов в массив нот: Note[] ParseMelody(string melody). Цифры от 0 до 9 соответствуют 10 последовательным нотам на Ваше усмотрение, пробел – паузе. Продолжительность звучания нот одинаковая и устанавливается на Ваше усмотрение.
+///Создать статический класс Player с методом для последовательного воспроизведения массива нот с помощью метода Console.Beep:
+///void Play(Note[] notes).
+///Пример использования описанных классов:
+///string melody = "987 876 765 654 543 432 111 8";
+///Note[] notes = MelodyParser.ParseMelody(melody);
+///Player.Play(notes);
+/// </summary>
+namespace Task2
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string melody = "987 876 765 654 543 432 111 8";
+            Note[] notes = MelodyParser.ParseMelody(melody);
+            Player.Play(notes);
+        }
+    }
+    static class Player
+    {
+        public static void Play(Note[] notes)
+        {
+            foreach (Note note in notes)
+            {
+                if (note.Freq < 37)
+                    Console.Beep(37, 300);
+                else if (note.Freq > 32767) Console.Beep(note.Freq, 32767);
+                else Console.Beep(note.Freq, 300);
+            }
+        }
+    }
+    class Note
+    {
+        public int Freq { get; set; }
+        public Note(int freq)
+        {
+            Freq = freq;
+        }
+    }
+    class MelodyParser
+    {
+        public static Note[] ParseMelody(string melody)
+        {
+            int count;
+            string[] strNotes = melody.Split(' ');
+            count = strNotes.Length;
+            Note[] notes = new Note[count];
+            for (int i = 0; i < count; i++)
+            {
+                notes[i] = new Note(Convert.ToInt32(strNotes[i]));
+            }
+            return notes;
+        }
+    }
+}
